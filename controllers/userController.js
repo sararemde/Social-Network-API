65711ee3c2be62bd92e145ed3384d212b56d76da
sara remde <sara@remde.us>
2022-11-09 13:22:33 -0600

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,23 @@
 const User = require('../models/user');
+
+module.exports = {
+    getUsers(req, res) {
+        User.find({})
+        .then((courses) => res.json(courses))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userId })
+        .populate([
+            { }
+        ])
+    .select('__v')
+    .then((course) => 
+        !course
+            ? res.status(404).json({ message: 'No user with that ID'})
+            : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+    },
+}
